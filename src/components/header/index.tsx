d20da4fb9760a4b57ebc08fe0a4529015be019c4
Renee Casaus <shawncasaus@Shawns-MacBook-Pro.local>
2022-10-10 23:57:06 +0100

    FIX: fixed eslint problems with code

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,13 @@
-import * as React from "react"
+import * as React from 'react';
 
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+	color: '#232129',
+	padding: 96,
+	fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
 
-const Header = () => {
-  return (
-      <h1>Header</h1>
-  )
-}
+const Header = () => (
+	<h1>Header</h1>
+);
 
 export default Header;
