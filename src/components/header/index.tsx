45281830c1c4f1bf6b7a9bdcc1cdd24c5d18b18c
Renee Casaus <shawncasaus@Shawns-MacBook-Pro.local>
2022-10-10 22:57:50 +0100

    FEAT: first commit to github

---
+++ src/components/header/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+const Header = () => {
+  return (
+      <h1>Header</h1>
+  )
+}
+
+export default Header;
