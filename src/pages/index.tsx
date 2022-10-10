f70c1ce9d5a4a5ee6ba354f603d3cd0a255dc6b8
Renee Casaus <shawncasaus@Shawns-MacBook-Pro.local>
2022-10-10 23:32:33 +0100

    FEAT: added prettier and eslint configurations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
+import * as React from 'react';
+import type {HeadFC} from 'gatsby';
 
 const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+	color: '#232129',
+	padding: 96,
+	fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
 
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1>Welcome to my portfolio</h1>
-    </main>
-  )
-}
+const IndexPage = () => (
+	<main style={pageStyles}>
+		<h1>Welcome to my portfolio</h1>
+	</main>
+);
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
